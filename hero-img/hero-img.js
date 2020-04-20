cd5d392dae0df0253b30839ea73378c0c9ee2b04
Timothy McGowen <mcgowen17@yahoo.com>
2020-04-20 16:25:46 -0700

    Website update

--- hero-img/hero-img.js
+++ hero-img/hero-img.js
@@ -1,20 +1,20 @@
-let hero_img = {
-    title: "Out Now!",
-    subtitle: "Eldritch Knight",
-    callout: "Buy Now",
-    callout_link: "amazon.com",
-    img: {
-        src: "./assets/HighResEK.png",
-        alt: "textless cover Eldritch Knight",
-    }
-}
+// let hero_img = {
+//     title: "Out Now!",
+//     subtitle: "Eldritch Knight",
+//     callout: "Buy Now",
+//     callout_link: "amazon.com",
+//     img: {
+//         src: "./assets/HighResEK.png",
+//         alt: "textless cover Eldritch Knight",
+//     }
+// }
 
     // Connects to Nav
-    document.querySelector(".hero-img").innerHTML = `
-    <img src=${hero_img.img.src} alt=${hero_img.img.alt} class="ek-img">
-    <div class="text-block">
-    <h3>${hero_img.title}</h3>
-    <a href=${hero_img.callout_link}><p>${hero_img.callout}</p></a>
-    <h2>${hero_img.subtitle}</h2>
-    </div>
-    `;
+    // document.querySelector(".hero-img").innerHTML = `
+    // <img src=${hero_img.img.src} alt=${hero_img.img.alt} class="ek-img">
+    // <div class="text-block">
+    // <h3>${hero_img.title}</h3>
+    // <a href=${hero_img.callout_link}><p>${hero_img.callout}</p></a>
+    // <h2>${hero_img.subtitle}</h2>
+    // </div>
+    // `;
--- nav/nav.js
+++ nav/nav.js
@@ -1,15 +1,23 @@
-let nav = {
-        name: "Timothy McGowen",
-        nav_items: {
-            home: "Home",
-            more: "More",
-            books: "Books",
-            newsletter: "Newsletter",
-            about: "About"
-        }
-}
+// let nav = {
+//         name: "Timothy McGowen",
+//         nav_items: {
+//             home: "Home",
+//             more: "More",
+//             books: "Books",
+//             newsletter: "Newsletter",
+//             about: "About"
+//         }
+// }
 
-    // Connects to Nav
-    document.querySelector(".nav").innerHTML = `
-    <h1>${nav.name}</h1> 
-    `;
+//     // Connects to Nav
+//     document.querySelector(".nav").innerHTML = `
+//     <h1>${nav.name}</h1> 
+//     `;
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
